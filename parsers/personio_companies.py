8fccdfaab45bc0def58accfa9fde5f26889b942f
henrythier <henry@thierhoff.rocks>
2023-08-30 23:21:01 +0200

    added models

--- parsers/personio_companies.py
+++ parsers/personio_companies.py
@@ -1,10 +1,64 @@
 import requests
 import xml.etree.ElementTree as ET
 import xmltodict
+from models import jobs
 
 ENDPOINT = "https://{company}.jobs.personio.de/xml?language=en"
+JOBENDPOINT = "https://{company}.jobs.personio.de/job/{id}?display=en"
 COMPANIES = ["recup","ostrom", "gridx"]
 
+'''
+Map strings to enums
+'''
+def map_category(category: str) -> jobs.Category:
+    if (category == "marketing_and_product"):
+        return jobs.Category.MARKETING
+    elif (category == "it_software"):
+        return jobs.Category.ENGINEERING
+    elif (category == "sales_and_business_development"):
+        return jobs.Category.SALES
+    elif (category == "administrative_and_clerical"):
+        return jobs.Category.ADMIN
+    elif (category == "production_and_operations"):
+        return jobs.Category.MANUFACTURING
+    elif (category == "other"):
+        return jobs.Category.OTHER
+    else:
+        return jobs.Category.UNKNOWN
+
+def map_seniority(seniority: str) -> jobs.Senority:
+    if (seniority == "student"):
+        return jobs.Senority.STUDENT
+    elif (seniority == "entry-level"):
+        return jobs.Senority.ENTRYLEVEL
+    elif (seniority == "experienced"):
+        return jobs.Senority.EXPERIENCED
+    else:
+        return jobs.Senority.UNKNOWN
+    
+def map_schedule(schedule: str) -> jobs.Schedule:
+    if (schedule == "part-time"):
+        return jobs.Schedule.PARTTIME
+    elif (schedule == "full-time"):
+        return jobs.Schedule.FULLTIME
+    else:
+        return jobs.Schedule.UNKNOWN
+    
+def map_experience(experience: str) -> jobs.Experience:
+    if (experience == "lt-1"):
+        return jobs.Experience.ENTRY
+    elif (experience == "1-2"):
+        return jobs.Experience.JUNIOR
+    elif (experience == "2-5"):
+        return jobs.Experience.MID
+    elif (experience == "5-7"):
+        return jobs.Experience.SENIOR
+    else:
+        return jobs.Experience.UNKNOWN
+
+'''
+Parse XML to Job Objects
+'''
 def get_jobs(company):
     company_endpoint = ENDPOINT.format(company=company)
     # Sending the GET request
@@ -26,7 +80,22 @@ def get_jobs(company):
             position_string = ET.tostring(p, encoding="utf-8").decode("utf-8")
             position_dict = xmltodict.parse(position_string)['position']
             position_dict.pop('jobDescriptions')
-            print(position_dict)
+
+            job_category = map_category(position_dict['occupationCategory'])
+            job_seniority = map_seniority(position_dict['seniority'])
+            job_schedule = map_schedule(position_dict['schedule'])
+            job_experience = map_experience(position_dict['yearsOfExperience'])
+
+            job = jobs.Job(
+                id=position_dict['id'],
+                title=position_dict['name'],
+                category=job_category,
+                seniority=job_seniority,
+                schedule=job_schedule,
+                experience=job_experience,
+                link=JOBENDPOINT.format(company=company, id=position_dict['id'])
+            )
+            print(job)
 
     else:
         print("Request for {company} failed with status code: {response.status_code}")
